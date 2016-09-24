52418a3c024db05ca3774803f23aa0276f616817
cdiez50 <c.deret@hotmail.fr>
2016-09-24 11:07:24 +0200

    Create tmp Dir and create function to remove tmp.txt file

--- __main__.py
+++ __main__.py
@@ -22,7 +22,7 @@ oQuestion = input("What is your question ? ")
 oInput=oQuestion.replace(" ", "_")
 
 # We put the curl's output inside a text.file
-with open('out-file.txt', 'w') as f:
+with open('tmp/out-file.txt', 'w') as f:
     oRequest = subprocess.call(['curl', '--silent', 'https://www.evi.com/q/' + oInput], stdout=f)
 
 # Convert it in str
@@ -30,19 +30,29 @@ oRequests = str(oRequest)
 
 # Here is the regex works bash on the output.txt
 # Need to create a Python one to avoir subprocess.call
-with open('retour.txt', 'w') as d:
-    oSed = subprocess.call(['bash', 'cut.bash', 'out-file.txt'], stdout=d)
+with open('tmp/retour.txt', 'w') as d:
+    oSed = subprocess.call(['bash', 'cut.bash', 'tmp/out-file.txt'], stdout=d)
 
 # In the debug mode, print the output of bash
-oContent = subprocess.call(['cat', 'retour.txt'])
+oContent = subprocess.call(['cat', 'tmp/retour.txt'])
 
 # Preparing to read
-with open ("retour.txt", "r") as retour:
+with open ("tmp/retour.txt", "r") as retour:
     data=retour.read()
 
 # Reding from the oSay_func()
 oSay_func(data)
 
+
+
+# After saying, we need to remove the two files : out-file.txt and retour.txt
+# We need to segment this function to another file.
+def purge(dir, pattern):
+    for f in os.listdir(dir):
+        if re.search(pattern, f):
+            os.remove(os.path.join(dir, f))
+
+purge('tmp/', 'txt')
 
 # Todo :
 # Actually, if the answer is not find on evi.com, we need to make a condition to print "not found message"

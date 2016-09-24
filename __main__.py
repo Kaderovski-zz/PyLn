4e234dce53d6ea2b3d5d0f3d866483c092e8db4e
cdiez50 <c.deret@hotmail.fr>
2016-09-24 10:00:09 +0200

    put comments in english

--- __main__.py
+++ __main__.py
@@ -3,44 +3,48 @@
 
 # -*- coding: utf-8 -*-
 
-# Liste des Import à prévoir pour le bon fonctionnement applicatif
+# List import
+# Have to clear it !
 import os, sys
 import subprocess
 import re
 
-# Définition de la function de lecture
+# Define the output voice as a function
 def oSay_func(arg):
     subprocess.call(['google_speech', '-l', 'en', arg, '-e', 'speed', '1.1'])
 
-# On attend l'entrée utilisateur
+# Asking user question :
 oQuestion = input("What is your question ? ")
 
 
-# On remplace avec underscore
-#print(oQuestion.replace (" ", "_"))
+# As we doing a curl from the evi.com/q/ we need to replace spaces with "_"
+# print(oQuestion.replace (" ", "_"))
 oInput=oQuestion.replace(" ", "_")
 
-# On indique le retour du curl en text "out-file.txt"
+# We put the curl's output inside a text.file
 with open('out-file.txt', 'w') as f:
     oRequest = subprocess.call(['curl', '--silent', 'https://www.evi.com/q/' + oInput], stdout=f)
 
-# On converti en str
+# Convert it in str
 oRequests = str(oRequest)
 
-# On va lire le retour.txt en faisant le travail de sortie du cut.bash
+# Here is the regex works bash on the output.txt
+# Need to create a Python one to avoir subprocess.call
 with open('retour.txt', 'w') as d:
     oSed = subprocess.call(['bash', 'cut.bash', 'out-file.txt'], stdout=d)
 
-# Pour afficher la réponse également
+# In the debug mode, print the output of bash
 oContent = subprocess.call(['cat', 'retour.txt'])
 
-# Préparation du fichier retour à lire
+# Preparing to read
 with open ("retour.txt", "r") as retour:
     data=retour.read()
 
-# Lecture
+# Reding from the oSay_func()
 oSay_func(data)
 
 
-# il faut préparer l'éventualité de la réponse non trouvée
+# Todo :
+# Actually, if the answer is not find on evi.com, we need to make a condition to print "not found message"
+# Or read it with the oSay_func()
 # tk_not_answered

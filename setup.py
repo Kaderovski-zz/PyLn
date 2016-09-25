0baba7650622a41763e526a79ab4d33d67167b4c
cdiez50 <c.deret@hotmail.fr>
2016-09-25 11:14:08 +0200

    Create PyLn Dir and setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+# Dev par Cdiez 50
+
+# -*- coding: utf-8 -*-
+
+import os
+
+
+setup(
+    name="PyLn",
+    version='0.0.1',
+    description='PyLn is a Python AI bot project',
+    url='https://github.com/Cdiez50/PyLn',
+    license='GPL3',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+        'Operating System :: OS Independent'],
+    author='Corentin Deret',
+    author_email='c.deret@hotmail.fr',
+)
